2c840343c77a2d5eba8caabceff27b0132f3eeff
Nicholas Giamblanco <gdev.engineering@gmail.com>
2018-02-06 14:57:26 -0500

    ~ added search visualization

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -7,14 +7,16 @@ var text_decode = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_-+,.:";
 text_decode = text_decode.split("");
 
 var search_query = "";
+var summary=[]
 
-var font_size = 10;
+var font_size = 50;
 var columns;
 
 //an array of drops - one per column
 var drops = [];
-var pause_rand_draw = false;
+var draw_state = 0;
 
+var rotation = 0;
 
 
 /* Load Document */
@@ -37,21 +39,28 @@ function draw_document() {
 	
 }
 
+function send_query(query) {
+	var status = 1;
+	draw_state = 2;
+	return status;
+}
+
 function get_search_contents() {
-	$("#search").on("change keyup paste",function() {
+	$("#search").on("change keyup paste",function(e) {
 		if($("#search").val()) {
-			if(!pause_rand_draw) {
-				pause_rand_draw = 	true;
+			if(draw_state == 0) {
+				draw_state = 	1;
 			}
 		} else {
-			if(pause_rand_draw) {
-				pause_rand_draw = 	false;
+			if(draw_state == 1 || draw_state == 2) {
+				draw_state = 	0;
 			}			
 		}
 		search_query 	=	$("#search").val();
-		console.log(search_query);
+		if(e.which == 13 && $("#search").val()) {
+        	send_query(search_query);
+    	}
 		search_query 	= 	search_query.split("");
-		console.log(search_query);
 	});
 }
 
@@ -60,50 +69,89 @@ function rand_draw(cnvs, ctx, font_size, text_decode, drops) {
 
 	//Black BG for the canvas
 	//translucent BG to show trail
+
 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 	ctx.fillRect(0, 0, cnvs.width, cnvs.height);
 	
-	ctx.fillStyle = "#AFF"; //green text
 	ctx.font = font_size + "px monospace";
 
-	if(!pause_rand_draw) {
+	switch(draw_state) {
+		case 0:
+			ctx.fillStyle = "#AFF";
+			//looping over drops
+			for(var i = 0; i < drops.length; i++) {
+				var text = text_decode[Math.floor(Math.random()*text_decode.length)];
 
-		//looping over drops
-		for(var i = 0; i < drops.length; i++) {
-			var text = text_decode[Math.floor(Math.random()*text_decode.length)];
+				//y = i*font_size, x = value of drops[i]*font_size
+				ctx.fillText(text, drops[i]*font_size, i*font_size);
+				
+				//sending the drop back to the top randomly after it has crossed the screen
+				//adding a randomness to the reset to make the drops scattered on the X axis
+				if(drops[i]*font_size > cnvs.width && Math.random() > 0.975)
+					drops[i] = -1;
+				
+				//incrementing X coordinate
+				drops[i]++;
+			}
+			break;
 
-			//y = i*font_size, x = value of drops[i]*font_size
-			ctx.fillText(text, drops[i]*font_size, i*font_size);
-			
-			//sending the drop back to the top randomly after it has crossed the screen
-			//adding a randomness to the reset to make the drops scattered on the X axis
-			if(drops[i]*font_size > cnvs.width && Math.random() > 0.975)
-				drops[i] = 0;
-			
-			//incrementing X coordinate
-			drops[i]++;
-		}
-	} else {
-		//looping over drops
-		for(var i = 0; i < drops.length; i++) {
-			if(Math.random() > 0.975) {
-				for(var where = 0; where < search_query.length; ++where) {
+		case 1:
+			//looping over drops
+			ctx.fillStyle = "#0F0";
+			status_q = " Querying ["+$("#search").val()+"]...";
+			status_q = status_q.split("");
+			for(var qi = 0; qi < status_q.length; ++ qi) {
+				t=status_q[qi];
+				ctx.fillText(t, qi*font_size, 1*font_size);
+			}
+			for(var i = 0; i < drops.length; i++) {
+				if(Math.random() > 0.98) {
+					ctx.fillText("*", 0, i*font_size);
 
-					var text = search_query[where];
-
-					//y = i*font_size, x = value of drops[i]*font_size
-					ctx.fillText(text, drops[i]*font_size, i*font_size);
 					
-					if(drops[i]*font_size > cnvs.width && Math.random() > 0.975)
-						drops[i] = 0;
-					
-					//incrementing X coordinate
-					drops[i]++;
+
+					for(var where = 0; where < search_query.length; ++where) {
+
+						var text = search_query[where];
+
+						//y = i*font_size, x = value of drops[i]*font_size
+						ctx.fillText(text, drops[i]*font_size, i*font_size);
+						
+						if(drops[i]*font_size > cnvs.width && Math.random() > 0.975)
+							drops[i] = 0;
+						
+						//incrementing X coordinate
+						drops[i]++;
+					}
 				}
 			}
-		}
-		
+			break;
 
+		case 2:
+			ctx.font = font_size + "px monospace";
+			xoffset = font_size*window.innerWidth*0.04;
+			yoffset = font_size*10;
+			ctx.fillStyle = "rgba(0, 0, 0, 0.01)";
+			ctx.fillRect(0, yoffset-1*font_size, font_size, font_size*summary.length);
+			if(rotation == 29) {
+				ctx.fillStyle = "#FFF";
+				ctx.fillRect(0, yoffset-1*font_size, font_size, font_size*summary.length);
+			}			
+			for(var i=0; i < summary.length; ++i) {
+
+
+				for(var j=0; j < summary[i].length; ++j) {
+					
+					var text = summary[i][j];
+					ctx.fillStyle = "#FF0";
+					ctx.fillText(text, j*font_size+xoffset, i*font_size+yoffset);
+				}
+
+
+			}
+			rotation = rotation +1;
+			rotation = rotation % 30;
+			break;
 	}
 }
 
@@ -121,6 +169,9 @@ $(function() {
 	cnvs.height = 400;
 	cnvs.width = window.innerWidth;
 
+	for(var i = 0; i < 20; ++i) {
+		summary[i] = "This is a sentence "+i+".";
+	}
 
 	font_size = 10;
 	columns = cnvs.width/font_size;
@@ -131,7 +182,7 @@ $(function() {
 		drops[x] = (Math.random()*1000)%columns; 
 	}
 
-	setInterval(function() {rand_draw(cnvs, ctx, font_size, text_decode, drops)}, 33);
+	setInterval(function() {rand_draw(cnvs, ctx, font_size, text_decode, drops)}, 35);
 	get_search_contents()
 });
 
